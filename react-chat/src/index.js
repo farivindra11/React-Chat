0c03201af5616e2a3d2d81b5b0a9b8a2b7500028
fariv indra <farivind@gmail.com>
2020-09-07 01:49:58 +0700

    first commit

--- react-chat/src/index.js
+++ react-chat/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ChatBox from './components/ChatBox';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChatBox />
   </React.StrictMode>,
   document.getElementById('root')
 );
