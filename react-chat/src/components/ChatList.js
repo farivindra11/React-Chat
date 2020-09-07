2be869038017990e161610567bc3e7e05a06d433
fariv indra <farivind@gmail.com>
2020-09-08 03:28:40 +0700

    initial chatlist

--- react-chat/src/components/ChatList.js
+++ react-chat/src/components/ChatList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ChatItem from './ChatItem'
 
 export default function ChatList(props) {
-    const list = props.data.map(item => <ChatItem title={item.title} />)
+    const list = props.data.map(item => <ChatItem title={item.title} delete={() => props.remove(item.id)} />)
 
     return (
         <ol>{list}</ol>
