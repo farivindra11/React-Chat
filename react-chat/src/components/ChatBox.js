7d455908d0ab939667b1380f3099dfb4c61abf60
fariv indra <farivind@gmail.com>
2020-09-08 03:25:41 +0700

    initial chatbox

--- react-chat/src/components/ChatBox.js
+++ react-chat/src/components/ChatBox.js
@@ -3,16 +3,32 @@ import ChatForm from './ChatForm'
 import ChatList from './ChatList'
 
 export default class ChatBox extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state = {data: [{title : "belajar coding"}]}
+        this.state = { data: [] }
+        this.addChat = this.addChat.bind(this)
+        this.removeChat = this.removeChat.bind(this)
     }
+
+    addChat(title) {
+        const id = Date.now()
+        this.setState((state, props) => ({
+            data: [{id, title }, ...state.data]
+        }));
+    }
+
+    removeChat(id){
+        this.setState((state, props) => ({
+            data: state.data.filter(item => item.id !=id)
+          }));
+    }
+
     render() {
         return (
             <div>
-                <ChatForm />
-                <ChatList data={this.state.data} />
+                <ChatForm add={this.addChat} />
+                <ChatList data={this.state.data}  remove={this.removeChat} />
             </div>
         )
-    } 
+    }
 }
