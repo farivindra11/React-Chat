1120b735893e400659071c16c5ae0aaa7b7a25b1
fariv indra <farivind@gmail.com>
2020-09-08 03:26:37 +0700

    initial chatform

--- react-chat/src/components/ChatForm.js
+++ react-chat/src/components/ChatForm.js
@@ -5,14 +5,17 @@ export default class ChatForm extends React.Component {
     constructor(props) {
       super(props);
       this.state = {value: ''};
+
       this.handleChange = this.handleChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
     }
   
     handleChange(event) {    this.setState({value: event.target.value});  }
+
     handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
       event.preventDefault();
+      this.props.add(this.state.value);
+      this.setState({value: ''})
     }
   
     render() {
