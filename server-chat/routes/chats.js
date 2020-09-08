36cca0307beda7e81ca4f0c2affeefa8abcb1f06
fariv indra <farivind@gmail.com>
2020-09-08 16:53:19 +0700

    routes post

--- server-chat/routes/chats.js
+++ server-chat/routes/chats.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var router = express.Router();
 const Chat = require('../models/Chat')
-
+// console.log(Chat);
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  Chat.find().then((data) => {
+  Chat.find({}).then((data)=>{
     res.json(data)
-  }).catch((err) => {
+  }).catch((err)=>{
+    res.status(500).json(err)
+  })
+});
+
+router.post('/', function(req, res, next) {
+  const {id, name, message} = req.body;
+  Chat.create({id, name, message}).then((data)=>{
+    res.json(data)
+  }).catch((err)=>{
     res.status(500).json(err)
   })
 });
