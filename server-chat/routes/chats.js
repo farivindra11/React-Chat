ae37743d9bd8bd68c476b9b05090d9b5a6c528ca
fariv indra <farivind@gmail.com>
2020-09-09 19:24:12 +0700

    routes delete

--- server-chat/routes/chats.js
+++ server-chat/routes/chats.js
@@ -3,19 +3,28 @@ var router = express.Router();
 const Chat = require('../models/Chat')
 // console.log(Chat);
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  Chat.find({}).then((data)=>{
+router.get('/', function (req, res, next) {
+  Chat.find({}).then((data) => {
     res.json(data)
-  }).catch((err)=>{
+  }).catch((err) => {
     res.status(500).json(err)
   })
 });
 
-router.post('/', function(req, res, next) {
-  const {id, name, message} = req.body;
-  Chat.create({id, name, message}).then((data)=>{
-    res.json(data)
-  }).catch((err)=>{
+router.post('/', function (req, res, next) {
+  const { id, name, message } = req.body;
+  Chat.create({ id, name, message }).then((data) => {
+    res.status(201).json(data)
+  }).catch((err) => {
+    res.status(500).json(err)
+  })
+});
+
+router.delete('/:id', function (req, res, next) {
+  const { id } = req.params;
+  Chat.findOneAndRemove({id: Number(id)}).then((data) => {
+    res.status(201).json(data)
+  }).catch((err) => {
     res.status(500).json(err)
   })
 });
