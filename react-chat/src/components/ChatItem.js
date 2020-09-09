654a59ab33433daec9ec4ed7f30a2359012ad290
fariv indra <farivind@gmail.com>
2020-09-09 20:05:41 +0700

    chat item

--- react-chat/src/components/ChatItem.js
+++ react-chat/src/components/ChatItem.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 export default function ChatItem(props) {
     return (
-        <li>{props.title} 
+        <li>
+            <h2>{props.name}</h2>
+            <p>{props.message}</p>
             <button onClick={props.delete}>Delete</button>
         </li>
     )
