405376e03fe86fef0a34838e8941c4ad4672c1ec
fariv indra <farivind@gmail.com>
2020-09-10 21:55:52 +0700

    add css

--- react-chat/src/components/ChatItem.js
+++ react-chat/src/components/ChatItem.js
@@ -6,7 +6,7 @@ export default function ChatItem(props) {
             <h2>{props.name}</h2>
             <p>{props.message}</p>
             {!props.sent && <p style ={{color: 'red', 'font-size': '8pt'}}>network failed</p>}
-            <button onClick={props.sent ? props.delete : props.resend}>{props.sent ? 'hapus' : 'kirim ulang'}</button>
+            <button className="btn btn-outline-danger" onClick={props.sent ? props.delete : props.resend}>{props.sent ? 'hapus' : 'kirim ulang'}</button>
         </li>
     )
 }
